5df880de70b4c6737a4c0753ca78a13125f50bbe
Yenduri <Yenduri@HP>
2017-11-12 12:19:18 +0530

    Added New Project for Spring Boot_Hibernate_JPA

---
+++ Boot_Hib_Jpa/src/main/java/com/jsp/ms/BootHibJpaApplication.java
@@ -0,0 +1,12 @@
+package com.jsp.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootHibJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootHibJpaApplication.class, args);
+	}
+}
